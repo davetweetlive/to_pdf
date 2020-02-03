7930203055f3d36bd0996bdb1933c58534538da3
Dave Augustus <davetweetlive@gmail.com>
2020-02-04 00:01:09 +0530

    Able to read files from browser upload

---
+++ convert_to_pdf/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+
+def index(request):
+    if request.method == "POST":
+        upload = request.FILES['my_uploaded_file']
+        print(upload)
+
+    return render(request, "index.html",{})
+    # return HttpResponse("Please upload a file!")
