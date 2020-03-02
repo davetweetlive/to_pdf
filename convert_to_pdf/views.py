2d2ad422aa1b46518a6cc40d1b1a6f2a605e426f
Dave Augustus <davetweetlive@gmail.com>
2020-03-03 01:31:19 +0530

    anonymous commit

--- convert_to_pdf/views.py
+++ convert_to_pdf/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+# from django.http import HttpResponse
+import pdfkit
+import filetype
 # Create your views here.
 
 def index(request):
     if request.method == "POST":
         upload = request.FILES['my_uploaded_file']
-        print(upload.type)
+        kind = filetype.guess(upload)
+
+        # Getting the name of output file
+        output_file = str(upload).split(".")[0] + ".pdf"
+        pdfkit.from_file(upload,output_file)
         print(upload)
+        print(kind)
+        print(output_file)
 
     return render(request, "index.html",{})
     # return HttpResponse("Please upload a file!")
